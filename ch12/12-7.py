31cf98a7225e779c3819dbe39ce79c08cbafaf55
bluvory <69047310+bluvory@users.noreply.github.com>
2021-01-25 23:08:22 +0900

    Update 12-7.py

--- ch12/12-7.py
+++ ch12/12-7.py
@@ -1,6 +1,14 @@
 n = int(input())
 left = 0
 right = 0
+
 for i in range(len(str(n))):
   if i < len(str(n))/2:
-    left = 
+    left += int(str(n)[i])
+  else:
+    right += int(str(n)[i])
+
+if right==left:
+  print("LUCKY")
+else:
+  print("READY")
