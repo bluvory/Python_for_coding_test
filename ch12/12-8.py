08a61750738b0b8b755ad33bce8285cf74d44e3c
bluvory <69047310+bluvory@users.noreply.github.com>
2021-01-25 23:32:44 +0900

    Update 12-8.py

--- ch12/12-8.py
+++ ch12/12-8.py
@@ -6,4 +6,8 @@ for i in data:
   if '0' <= i <= '9':
     res += int(i)
 
+for i in range(len(data)-1, -1, -1):
+  if '0' <= data[i] <= '9':
+    data.remove(data[i])
+
 print(''.join(data)+str(res))
